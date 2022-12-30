1084bf26cdc4a34f52763e4932f3566aa06dd73c
unknown <udaykumar.n040796@example.com>
2022-12-30 21:59:42 +0530

    component communication example

--- src/app/student-details/student-details.component.ts
+++ src/app/student-details/student-details.component.ts
@@ -1,18 +1,20 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-student-details',
   templateUrl: './student-details.component.html',
   styleUrls: ['./student-details.component.css']
 })
-export class StudentDetailsComponent implements OnInit{
-  @Input() recievedSttementFromStudents = '';
-  empName : string = 'Meenakshi';
-  @Output() valueToBeSendToParent = new EventEmitter();
+export class StudentDetailsComponent implements OnInit,OnChanges{
 
-  ngOnInit(){
-    console.log('the value gettting from parent is : ',this.recievedSttementFromStudents);
-    this.valueToBeSendToParent.emit(this.empName);
+  @Input() recievedStudent: any;
+
+  ngOnInit() {
+      
+  }
+
+  ngOnChanges(){
+    console.log('recieved Dta From Student: ',this.recievedStudent);
   }
 
 }
--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-students',
-  templateUrl: './students.component.html',
-  styleUrls: ['./students.component.css']
+  selector: "app-students",
+  templateUrl: "./students.component.html",
+  styleUrls: ["./students.component.css"],
 })
 export class StudentsComponent {
-   statement : string = 'India is my country';
-   sendNow(event:any){
-    console.log('the value getting from child :  ',event);
-   }
+
+  selectedStudent : any ;
+  students: any[] = [
+    {
+      id: 120,
+      name: "uday",
+      age: 25,
+    },
+    {
+      id: 121,
+      name: "nandhini",
+      age: 22,
+    },
+    {
+      id: 122,
+      name: "manisha",
+      age: 26,
+    },
+    {
+      id: 123,
+      name: "ravi",
+      age: 25,
+    },
+  ];
+
+  ngOnInit(){
+    this.onClick(this.students[0]);
+  }
+
+  onClick(student:any){
+   console.log(student);
+   this.selectedStudent = student;
+  }
 }
