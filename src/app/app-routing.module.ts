4a960c8387bd0012017860f02a6a4c38dbbf2f6c
unknown <udaykumar.n040796@example.com>
2023-02-08 07:41:25 +0530

    xyz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,8 @@
 import { NgModule } from '@angular/core';
+
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { AccountComponent } from './account/account.component';
-import { CareerComponent } from './career/career.component';
-import { ContactComponent } from './contact/contact.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { SigninComponent } from './signin/signin.component';
-import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  {path : '',redirectTo : '/header/home' , pathMatch : 'full'},
-  {path : 'header',component : HeaderComponent,children : [
-    {path : 'home',component : HomeComponent},
-    {path : 'about' , component : AboutComponent},
-    {path : 'career' , component : CareerComponent},
-    {path : 'account', component : AccountComponent,children : [
-      {path : 'signin' , component : SigninComponent},
-      {path : 'signup',component : SignupComponent}
-    ]},
-    {path : 'contact', component : ContactComponent},
-    {path: '**' ,component : PageNotFoundComponent}
-  ]},
-  {path: '**' ,component : PageNotFoundComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-import { StudentsComponent } from './students/students.component';
-import { StudentDetailsComponent } from './student-details/student-details.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { CareerComponent } from './career/career.component';
-import { AccountComponent } from './account/account.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { DitrsictsComponent } from './ditrsicts/ditrsicts.component';
 import { UserService } from './user.service';
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SigninComponent } from './signin/signin.component';
-import { SignupComponent } from './signup/signup.component';
 import {MatDialogModule} from '@angular/material/dialog';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentsComponent,
-    StudentDetailsComponent,
-    HeaderComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent,
-    CareerComponent,
-    AccountComponent,
-    PageNotFoundComponent,
-    DitrsictsComponent,
-    SigninComponent,
-    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule,
     MatDialogModule
   ],
   providers: [UserService],
